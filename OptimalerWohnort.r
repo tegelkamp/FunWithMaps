70643ded051712471d5daa22fb970d89d0f2837f
Christian Tegelkamp <christian.tegelkamp@gmail.com>
2017-10-10 20:45:47 +0200

    Fahrtzeiten mit dem Auto

--- OptimalerWohnort.r
+++ OptimalerWohnort.r
@@ -4,7 +4,7 @@
 #Notwendige Pakete installieren
 #install.packages("ggmap")
 #install.packages("placement")
-install.packages("googleway")
+#install.packages("googleway")
 
 #Karte laden und erste Fahrtzeit ausprobieren
 require(ggmap)
@@ -13,27 +13,45 @@ require(googleway)
 my.apikey<-'AIzaSyAGt0UMnSW956R_GvWijJysOBPrk71GGUk'
 
 #Adressen festlegen
-adress.work<-list(c("Ebhardtstraße 6, 30159 Hannover, Germany")
-                 , c("Venloer Str. 14, 50672 Köln, Germany"))
+adress.work<-c("Ebhardtstraße 6, 30159 Hannover, Germany"
+              ,"Venloer Str. 14, 50672 Köln, Germany")
 
-adress.home<-list(c("Bistritzer Str. 92, 50858 Köln, Germany")
-                 ,c("Brandenburger Str. 13, 50668 Köln, Germany"))
+adress.home<-c("Bistritzer Str. 92, 50858 Köln, Germany"
+              , "Brandenburger Str. 13, 50668 Köln, Germany")
 
 
 adress.station<-list(c("50.938158,6.845206"),
                      c("Bahnhof Köln Lövenich"),
                      c("Köln Hauptbahnhof, Germany"))
 
+my.routes <- expand.grid(from = adress.home
+                         , to = adress.work
+                         , stringsAsFactors = FALSE)
+
 #Fahrtzeit mit Auto und Verkehr
-tmp<-google_distance(origins=adress.home[1]
-                ,destinations=adress.work[1]
+my.routes$morning<-
+    sapply(1:length(my.routes$from),
+          function(x) {google_distance(origins=my.routes$from[x]
+                ,destinations=my.routes$to[x]
                 ,mode='driving'
                 ,departure_time = as.POSIXct("2017-11-13 07:00:00"
-                                             , format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+                                             , format = "%Y-%m-%d %H:%M:%S", tz = "Europe/Berlin")
                 ,key = my.apikey
                 ,traffic_model = "pessimistic"
                 ,simplify = TRUE)$rows$elements[[1]][3]$duration_in_traffic$value
+          })
 
+my.routes$evening<-
+  sapply(1:length(my.routes$from),
+         function(x) {google_distance(origins=my.routes$to[x]
+                                      ,destinations=my.routes$from[x]
+                                      ,mode='driving'
+                                      ,departure_time = as.POSIXct("2017-11-13 18:00:00"
+                                                                   , format = "%Y-%m-%d %H:%M:%S", tz = "Europe/Berlin")
+                                      ,key = my.apikey
+                                      ,traffic_model = "pessimistic"
+                                      ,simplify = TRUE)$rows$elements[[1]][3]$duration_in_traffic$value
+         })
 
 #Laufzeit zum Bahnhof
 mapdist(from=adress.home
